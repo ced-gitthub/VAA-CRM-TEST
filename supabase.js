fdef01b2a30800617ffb5a4348b70729ca9f4416
ced-gitthub <bernardocedric20@gmail.com>
2026-05-25 19:32:13 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // ── PASTE YOUR SUPABASE CREDENTIALS HERE ──────────────────────
 const SUPABASE_URL = 'https://nygicyifpjlubbremklt.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_wQ-NAO8yyhk6Y5jshcMX3A_dP5us_Ja';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55Z2ljeWlmcGpsdWJicmVta2x0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2ODM4NTYsImV4cCI6MjA5NTI1OTg1Nn0.E3V-YktE2e1N5OekOU7wgTpryqPA7RTpLyGHOGxKZng';
 // ──────────────────────────────────────────────────────────────
 
 const { createClient } = supabase;
